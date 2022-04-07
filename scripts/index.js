b7d8aa97e7a618a6de8fd4d4b3cf9e5fc4dd07ec
Lindsay Forrister <lindsayforrister@gmail.com>
2022-04-07 15:14:19 -0500

    Adding in seek forward and seek back

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,69 @@
-console.log('yo what up')
+window.onload = function() {
+    
+    // First, I'll declare any global variables I'd like to have access to
+    const episodesUrl = 'http://localhost:1337/episodes'
+    const playButton = document.getElementById('play')
+    const pauseButton = document.getElementById('pause')
+    const seekForwardButton = document.getElementById('seekforward')
+    const seekBackButton = document.getElementById('seekback')
 
-//let's make sure I can connect to this endpoint
-const episodesUrl = 'http://localhost:1337/episodes'
 
-function fetchData(url, callback) {
-    fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then((data) => {
-          console.log('data', data)
-          callback(data)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
 
-fetchData(episodesUrl, podcastPlayer)
+    // Here is a generic fetch data function that I can use to grab what I need from the server
+    function fetchData(url, callback) {
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log('data', data)
+            callback(data)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
 
-function podcastPlayer(data) {
-    let audio = new Audio('http://localhost:1337' + data[0].audio)
+    // Now, I'll get the info I need from the episodes file and use that to pass appropriate data to my podcast player function
+    fetchData(episodesUrl, podcastPlayer)
 
-    console.log('testing', data)
-    const play = document.getElementById('play')
-    play.addEventListener('click', function() {
-        console.log("Play!")
+    function podcastPlayer(data) {
+        let audio = new Audio('http://localhost:1337' + data[0].audio)
+
+        //Event Listeners
+        playButton.addEventListener('click', playAudio.bind(this, audio))
+        pauseButton.addEventListener('click', pauseAudio.bind(this, audio))
+        seekForwardButton.addEventListener('click', sfAudio.bind(this, audio))
+        seekBackButton.addEventListener('click', sbAudio.bind(this, audio))
+    }
+
+    function playAudio(audio) {
         audio.play()
-    })
+    }
 
-    const pause = document.getElementById('pause')
-    pause.addEventListener('click', function() {
-        console.log("Pause!")
+    function pauseAudio(audio) {
         audio.pause()
-    })
+    }
+
+    function sfAudio(audio) {
+        console.log('fast forward', audio.currentTime)
+        audio.currentTime = audio.currentTime + 5
+        console.log('new time', audio.currentTime)
+        playAudio(audio)
+    }
+
+    function sbAudio(audio) {
+        console.log('back', audio.currentTime)
+        let newTime = audio.currentTime - 5
+
+        if (newTime < 0) {
+            newTime = 0
+        }
+
+        console.log('new time', newTime)
+        audio.currentTime = newTime
+        playAudio(audio)
+
+    }
 }
+
